1630ebc442901cfccc9840d03269c929c5977fa2
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-12 20:44:02 +0530

    Buid GUI and Digital clock Stuffs

---
+++ app/src/main/java/com/example/digitalWatch/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.digitalstopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextClock;
+
+public class MainActivity extends AppCompatActivity {
+    TextClock textClock, textampm, textdate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        textClock = findViewById(R.id.textclockid);
+        textampm = findViewById(R.id.textampmid);
+        textdate = findViewById(R.id.textdateid);
+
+        Typeface myfont = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/digital_font.ttf");
+
+        textClock.setTypeface(myfont);
+        textampm.setTypeface(myfont);
+        textdate.setTypeface(myfont);
+
+    }
+}
